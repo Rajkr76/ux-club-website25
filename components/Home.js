f894a7bac2915aa11a6d1d3d105c5fa1530d0a50
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2026-01-12 11:54:28 +0530

    removed the leader page component from home page

--- components/Home.js
+++ components/Home.js
@@ -7,13 +7,12 @@ export default function Home() {
       <div className="h-screen w-full bg-[#0e0e0e] relative text-[#eceae5] pt-3 font-[Neue]">
         <Nav/>
         <h1 className="font-[Socilo] text-[6.5rem] leading-none pt-10 text-center">
-          Club
+         Club
         </h1>
       </div>
-      <div className="h-screen relative w-full bg-[#0e0e0e] px-4 pt-2 text-[#eceae5] font-[Neue]">
-      <HeadPage  />
 
-      </div>
+      {/*  removed the leader component (HeadPage.js) from home page */}
+
     </>
   );
 }
